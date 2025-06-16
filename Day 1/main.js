44b506f19d9a59b3c4b8ec7f3091330626df4c1c
developermickey <mukesh.quazma@gmail.com>
2025-06-16 19:08:10 +0530

    comfrim code added

--- Day 1/main.js
+++ Day 1/main.js
@@ -9,3 +9,7 @@ const PI = 3.14;
 let age = prompt("How old are you?", 100);
 
 alert(`You are ${age} years old!`);
+
+let isBoss = confirm("Are you the boss?");
+
+alert(isBoss); // true if OK is pressed
